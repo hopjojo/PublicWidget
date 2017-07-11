c9f0552007e66b18928c38763bf31786950b622f
hopjojo <1120184367@.qq.com>
2017-07-11 16:52:39 +0800

    add update

---
+++ publicsdk/src/main/java/zj/neverland/publicsdk/update/Updater.java
@@ -0,0 +1,101 @@
+package zj.neverland.publicsdk.update;
+
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+import zj.neverland.publicsdk.R;
+
+
+/**
+ * Description：
+ *
+ * Created by cfoc on 2017/06/12
+ */
+
+public class Updater {
+
+    private static Updater instance;
+
+    private Updater() {
+        //
+    }
+
+    public synchronized static Updater get() {
+        if (instance == null) {
+            instance = new Updater();
+        }
+        return instance;
+    }
+
+    public Updater showLog(boolean log) {
+        Logger.get().setShowLog(log);
+        return this;
+    }
+
+    public void download(UpdaterConfig updaterConfig) {
+
+        if (!UpdaterUtils.checkDownloadState(updaterConfig.getContext())) {
+            Toast.makeText(updaterConfig.getContext(), R.string.system_download_component_disable, Toast.LENGTH_SHORT).show();
+            UpdaterUtils.showDownloadSetting(updaterConfig.getContext());
+            return;
+        }
+
+        long downloadId = UpdaterUtils.getLocalDownloadId(updaterConfig.getContext());
+        Logger.get().d("local download id is " + downloadId);
+        if (downloadId != -1L) {
+            FileDownloadManager fdm = FileDownloadManager.get();
+            //获取下载状态
+            int status = fdm.getDownloadStatus(updaterConfig.getContext(), downloadId);
+            switch (status) {
+                //下载成功
+                case DownloadManager.STATUS_SUCCESSFUL:
+                    Logger.get().d("downloadId=" + downloadId + " ,status = STATUS_SUCCESSFUL");
+                    Uri uri = fdm.getDownloadUri(updaterConfig.getContext(), downloadId);
+                    String path = fdm.getDownloadPath(updaterConfig.getContext(),downloadId);
+                    if (uri != null) {
+                        uri = Uri.parse(path);
+                        //本地的版本大于当前程序的版本直接安装
+                        if (UpdaterUtils.compare(updaterConfig.getContext(), uri.getPath())) {
+                            Logger.get().d("start install UI");
+                            UpdaterUtils.startInstall(updaterConfig.getContext(), uri);
+                            return;
+                        } else {
+                            //从FileDownloadManager中移除这个任务
+                            fdm.getDM(updaterConfig.getContext()).remove(downloadId);
+                        }
+                    }
+                    //重新下载
+                    startDownload(updaterConfig);
+                    break;
+                //下载失败
+                case DownloadManager.STATUS_FAILED:
+                    Logger.get().d("download failed " + downloadId);
+//                    startDownload(updaterConfig);
+                    Toast.makeText(updaterConfig.getContext(),"下载失败，请稍后再试！",Toast.LENGTH_SHORT).show();
+                    break;
+                case DownloadManager.STATUS_RUNNING:
+                    Logger.get().d("downloadId=" + downloadId + " ,status = STATUS_RUNNING");
+                    break;
+                case DownloadManager.STATUS_PENDING:
+                    Logger.get().d("downloadId=" + downloadId + " ,status = STATUS_PENDING");
+                    break;
+                case DownloadManager.STATUS_PAUSED:
+                    Logger.get().d("downloadId=" + downloadId + " ,status = STATUS_PAUSED");
+                    break;
+                default:
+                    Logger.get().d("downloadId=" + downloadId + " ,status = " + status);
+                    break;
+            }
+        } else {
+            startDownload(updaterConfig);
+        }
+    }
+
+    private void startDownload(UpdaterConfig updaterConfig) {
+        Toast.makeText(updaterConfig.getContext(),"后台正在进行更新....",Toast.LENGTH_SHORT).show();
+        long id = FileDownloadManager.get().startDownload(updaterConfig);
+        Logger.get().d("apk download start, downloadId is " + id);
+    }
+
+}
